100b72b24ae0e7e82779afa5077b4100ca94c43b
Fabrizio Lagioia <meandi958@gmail.com>
2024-06-17 16:41:38 +0200

    correzione

--- src/main/java/com/example/deploy02/Controller.java
+++ src/main/java/com/example/deploy02/Controller.java
@@ -1,14 +1,23 @@
 package com.example.deploy02;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class Controller {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("sum")
-    public int sum (@RequestParam int a, @RequestParam int b) {
+    public int sum () {
+        int a = Integer.parseInt(Objects.requireNonNull(environment.getProperty("a")));
+        int b = Integer.parseInt(Objects.requireNonNull(environment.getProperty("b")));
         return (a + b);
     }
 }
